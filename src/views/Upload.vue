6f30d8894f93bef3b1f948a5a22b08d230ba2ca7
abeselom-ab <abeselomtesfay@gmail.com>
2025-08-23 16:28:16 +0300

    Add Upload.vue

---
+++ src/views/Upload.vue
@@ -0,0 +1,10 @@
+<template>
+  <section class="px-4 md:px-20 py-10">
+    <h1 class="text-3xl font-bold mb-6 text-gray-900">Upload Your Skin Image</h1>
+    <ImageUpload />
+  </section>
+</template>
+
+<script setup>
+import ImageUpload from '@/components/ImageUpload.vue'
+</script>
