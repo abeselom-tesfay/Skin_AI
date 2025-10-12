c9ca3592ed2d67fc29190b3e245e9595d767e917
abeselom-ab <abeselomtesfay@gmail.com>
2025-10-12 16:48:13 +0300

    Update NavBar.vue

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -4,16 +4,16 @@
       <img src="@/assets/logo.png" alt="Logo" class="h-30 w-30"/>
     </div>
     <ul class="hidden md:flex space-x-6 text-white font-medium">
-      <li><a href="/#main" class="hover:text-blue-500 transition-colors underline">Main</a></li>
-      <li><a href="/#tech-stack" class="hover:text-blue-500 transition-colors underline">Technical Details</a></li>
-      <li><a href="/#risks" class="hover:text-blue-500 transition-colors underline">Risks Detection</a></li>
-      <li><a href="/#features" class="hover:text-blue-500 transition-colors underline">Features</a></li>
-      <li><a href="/#how-to-use" class="hover:text-blue-500 transition-colors underline">How it works</a></li>
-      <li><a href="/samples" class="hover:text-blue-500 transition-colors underline">Samples</a></li>
-      <li><a href="/#ai" class="hover:text-blue-500 transition-colors underline">Artificial Intelligence</a></li>
-      <li><a href="/faq" class="hover:text-blue-500 transition-colors underline">FAQ</a></li>
-      <li><a href="/register" class="hover:text-blue-500 transition-colors underline">Register</a></li>
-      <li><a href="/login" class="hover:text-blue-500 transition-colors underline">Log In</a></li>
+      <li><a href="/#main" class="hover:text-cyan-300 transition-colors max-xl:hidden">Main</a></li>
+      <li><a href="/#tech-stack" class="hover:text-cyan-300 transition-colors">Technical Details</a></li>
+      <li><a href="/#risks" class="hover:text-cyan-300 transition-colors max-xl:hidden">Risks Detection</a></li>
+      <li><a href="/#features" class="hover:text-cyan-300 transition-colors">Features</a></li>
+      <li><a href="/#how-to-use" class="hover:text-cyan-300 transition-colors">How it works</a></li>
+      <li><a href="/samples" class="hover:text-cyan-300 transition-colors">Samples</a></li>
+      <li><a href="/#ai" class="hover:text-cyan-300 transition-colors max-xl:hidden">Artificial Intelligence</a></li>
+      <li><a href="/faq" class="hover:text-cyan-300 transition-colors">FAQ</a></li>
+      <li><a href="/register" class="hover:text-cyan-300 transition-colors max-xl:hidden">Register</a></li>
+      <li><a href="/login" class="hover:text-cyan-300 transition-colors">Log In</a></li>
     </ul>
 
     <button 
@@ -37,15 +37,15 @@
       v-if="isOpen"
       class="absolute top-full left-0 w-full bg-cyan-700 shadow-md flex flex-col items-center space-y-4 py-4 md:hidden text-white"
     >
-      <a href="/#main" class="hover:text-cyan-500 underline" @click="isOpen = false">Main</a>
-      <a href="/#tech-stack" class="hover:text-blue-500 transition-colors underline" @click="isOpen = false">Technical Details</a>
-      <a href="/#risks" class="hover:text-cyan-500 underline" @click="isOpen = false">Risks</a>
-      <a href="/#features" class="hover:text-cyan-500 underline" @click="isOpen = false">Detection Features</a>
-      <a href="/#how-to-use" class="hover:text-cyan-500 underline" @click="isOpen = false">How it works</a>
-      <a href="/samples" class="hover:text-cyan-500 underline" @click="isOpen = false">Samples</a>
-      <a href="/#ai" class="hover:text-cyan-500 underline" @click="isOpen = false">Artificial Intelligence</a>
-      <a href="/faq" class="hover:text-cyan-500 underline" @click="isOpen = false">FAQ</a>
-      <a href="/register" class="hover:text-cyan-500 bg-cyan-800 rounded-full px-8 py-2" @click="isOpen = false">Register</a>
+      <a href="/#main" class="hover:text-cyan-300" @click="isOpen = false">Main</a>
+      <a href="/#tech-stack" class="hover:text-cyan-300 transition-colors" @click="isOpen = false">Technical Details</a>
+      <a href="/#risks" class="hover:text-cyan-300" @click="isOpen = false">Risks</a>
+      <a href="/#features" class="hover:text-cyan-300" @click="isOpen = false">Detection Features</a>
+      <a href="/#how-to-use" class="hover:text-cyan-300" @click="isOpen = false">How it works</a>
+      <a href="/samples" class="hover:text-cyan-300" @click="isOpen = false">Samples</a>
+      <a href="/#ai" class="hover:text-cyan-300" @click="isOpen = false">Artificial Intelligence</a>
+      <a href="/faq" class="hover:text-cyan-300" @click="isOpen = false">FAQ</a>
+      <a href="/register" class="hover:text-cyan-300 bg-cyan-800 rounded-full px-8 py-2" @click="isOpen = false">Register</a>
       <a href="/login" class="border border-white px-9 py-2 rounded-full" @click="isOpen = false">Log In</a>
     </div>
   </nav>
